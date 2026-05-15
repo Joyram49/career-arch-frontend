fa1e1810deefe74a0e8e2e5d22788e1e95dbacc0
Joy Ram Das <96444950+Joyram49@users.noreply.github.com>
2026-05-15 16:47:43 +0600

    Adds user auth service

---
+++ src/services/user/auth.ts
@@ -0,0 +1,192 @@
+import { envConfig } from '@config/envConfig';
+import { RegisterInput } from '@validations/auth.schema';
+
+interface IRegisterSuccessResult {
+  success: true;
+  message: string;
+}
+
+interface IRegisterErrorResult {
+  success: false;
+  message: string;
+  fieldErrors?: Array<{ field: string; message: string }>;
+}
+
+export type RegisterResult = IRegisterSuccessResult | IRegisterErrorResult;
+
+/* ─────────────────────────────────────────────
+   registerUser — Server Action
+   Calls POST /auth/user/register
+   No cookies set — backend only sends a verification email.
+   On success the user must check their inbox before they can log in.
+   ──────────────────────────────────────────── */
+
+export async function userRegistration(
+  payload: Pick<RegisterInput, 'firstName' | 'lastName' | 'email' | 'password'>,
+): Promise<RegisterResult> {
+  let response: Response;
+
+  //   send api request to the backend
+  try {
+    response = await fetch(`${envConfig.apiUrl}/auth/user/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: payload.firstName,
+        lastName: payload.lastName,
+        email: payload.email,
+        password: payload.password,
+      }),
+      cache: 'no-store',
+    });
+  } catch {
+    return {
+      success: false,
+      message: 'Unable to reach the server. Please check your connection and try again.',
+    };
+  }
+
+  let body: {
+    success: boolean;
+    message: string;
+    errors: Array<{ field: string; message: string }>;
+  };
+  try {
+    body = await response.json();
+  } catch (error) {
+    return {
+      success: false,
+      message: 'Unexpected server response. Please try again.',
+    };
+  }
+
+  //   guard error response
+  if (!response.ok) {
+    return {
+      success: false,
+      message: body.message ?? 'Registration failed. Please try again later',
+      fieldErrors: body.errors,
+    };
+  }
+
+  // send success response to the client
+  return {
+    success: true,
+    message: body.message,
+  };
+}
+
+/* ─────────────────────────────────────────────
+   resendVerification — Server Action
+   Calls POST /auth/user/resend-verification
+    backend will sends a verification email.
+   On success the user have to redirect with the url they will find in email
+   ──────────────────────────────────────────── */
+export async function resendVerificationEmail(
+  payload: Pick<RegisterInput, 'email'>,
+): Promise<RegisterResult> {
+  let response: Response;
+  //   send api request to the backend
+  try {
+    response = await fetch(`${envConfig.apiUrl}/auth/user/resend-verification`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: payload.email,
+      }),
+      cache: 'no-store',
+    });
+  } catch {
+    return {
+      success: false,
+      message: 'Unable to reach the server. Please check your connection and try again.',
+    };
+  }
+
+  let body: {
+    success: boolean;
+    message: string;
+    errors: Array<{ field: string; message: string }>;
+  };
+  try {
+    body = await response.json();
+  } catch (error) {
+    return {
+      success: false,
+      message: 'Unexpected server response. Please try again.',
+    };
+  }
+
+  //   guard error response
+  if (!response.ok) {
+    return {
+      success: false,
+      message: body.message ?? 'Verification failed. Please try again later',
+      fieldErrors: body.errors,
+    };
+  }
+
+  // send success response to the client
+  return {
+    success: true,
+    message: body.message,
+  };
+}
+
+/* ─────────────────────────────────────────────
+   verifyEmail — Server Action
+   Calls POST /auth/user/verify-email
+    backend will receive token to verify email.
+   On success the user can login their account
+   ──────────────────────────────────────────── */
+
+export async function verifyEmail(payload: { token: string }): Promise<RegisterResult> {
+  let response: Response;
+
+  try {
+    const url = new URL(`${envConfig.apiUrl}/auth/user/verify-email`);
+    url.searchParams.set('token', payload.token);
+
+    response = await fetch(url.toString(), {
+      method: 'GET',
+      cache: 'no-store',
+    });
+  } catch {
+    return {
+      success: false,
+      message: 'Unable to reach the server. Please check your connection and try again.',
+    };
+  }
+
+  let body: {
+    success: boolean;
+    message: string;
+    errors: Array<{ field: string; message: string }>;
+  };
+
+  try {
+    body = await response.json();
+  } catch {
+    return {
+      success: false,
+      message: 'Unexpected server response. Please try again.',
+    };
+  }
+
+  if (!response.ok) {
+    return {
+      success: false,
+      message: body.message ?? 'Verification failed. Please try again later',
+      fieldErrors: body.errors,
+    };
+  }
+
+  return {
+    success: true,
+    message: body.message,
+  };
+}
